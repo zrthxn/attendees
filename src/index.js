8373cfef1d540cca7c5834ae394e992353d03af9
Alisamar Husain <zrthxn@gmail.com>
2020-08-07 15:45:59 +0530

    Patch

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.engine('hbs',
   })
 )
 
-app.use(cookieParser())
+app.use(cookieParser("secret"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
